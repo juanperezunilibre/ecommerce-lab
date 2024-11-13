944a4c594f1676ac8cf17a650be23eb657b46331
Juan <juan.perez@unilibre.edu.co>
2024-11-13 10:12:15 -0500

    productos y local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,10 @@
-const productos = [
-    {
-        id: 1,
-        nombre: "Camisa a rayas",
-        descripcion: "Camisa tropial playera hombre",
-        imagen: "",
-        precio: 80000,
-        descuento: 50,
-        valoracion: 4,
-    }
-]
+import productos from "/productos.json" with { type: "json" };
+import {agregarProducto, carrito} from "/assets/js/localstorage.js"
 
 document.addEventListener("DOMContentLoaded", function() {
-
-    // Se referencia al section que contiene los productos
-    let grillaProductos = document.querySelector("#products-grid");
-
-    // Lógica de la card
-    // creamos el contenedor la card
-    let card = document.createElement("div")
-    card.classList.add("card");
-
-    // creamos la imagen
-    let imagen = document.createElement("img")
-    imagen.src = "https://www.patprimo.com/dw/image/v2/BHFM_PRD/on/demandware.static/-/Sites-storefront_catalog_pat/default/dw221a6097/images/hi-res/2022-2023/camisas-para-hombre-44012927-3559_1.jpg?sw=800&sh=960"
-    imagen.classList.add("card-img-top")
-    card.appendChild(imagen)
-
-    // cuerpo de la card
-    let cardBody = document.createElement("div")
-    cardBody.classList.add("card-body")
-    card.appendChild(cardBody)
-
-    // titulo de la card
-    let cardTitle = document.createElement("h5")
-    cardTitle.classList.add("card-title")
-    cardTitle.innerText = "Camisa a rayas"
-    cardBody.appendChild(cardTitle)
-
-    // descripcion de la card
-    let description = document.createElement("p")
-    description.classList.add("card-text")
-    description.innerText = "Camisa tropial playera hombre"
-    cardBody.appendChild(description)
-
-    // contenedor de precios
-    let priceWrapper = document.createElement("div")
-    priceWrapper.classList.add("price-content")
-    cardBody.appendChild(priceWrapper)
-
-    // precios
-    let price1 = document.createElement("span")
-    let price2 = document.createElement("span")
-
-    price1.innerText = "$ 75.000"
-    price2.innerText = "$ 80.000"
-    priceWrapper.append(price1, price2)
-
-    // valoracion
-    let rating = valoracionProducto(3)
-    cardBody.appendChild(rating)
-
-    // boton
-    let buttonWrapper = document.createElement("div")
-    buttonWrapper.classList.add("d-grid")
-
-    let button = document.createElement("button")
-    button.classList.add("btn")
-    button.classList.add("btn-primary")
-    button.innerText = "Agregar al carrito"
-    buttonWrapper.appendChild(button)
-
-    cardBody.appendChild(buttonWrapper)
-
-
-    // añadimos la card a la grilla de productos
-    grillaProductos.appendChild(card)
+    for (let producto of productos) {
+        dibujarTarjeta(producto)
+    }
 })
 
 function valoracionProducto(numero) {
@@ -95,6 +25,91 @@ function valoracionProducto(numero) {
     }
 
     return ratingWrapper
+}
+
+function dibujarTarjeta(producto) {
+    // Se referencia al section que contiene los productos
+    let grillaProductos = document.querySelector("#products-grid");
+
+    // Lógica de la card
+    // creamos el contenedor la card
+    let card = document.createElement("div")
+    card.classList.add("card");
+
+    // creamos la imagen
+    let imagen = document.createElement("img")
+    imagen.src = producto.imagen
+    imagen.classList.add("card-img-top")
+    card.appendChild(imagen)
+
+    // cuerpo de la card
+    let cardBody = document.createElement("div")
+    cardBody.classList.add("card-body")
+    card.appendChild(cardBody)
+
+    // titulo de la card
+    let cardTitle = document.createElement("h5")
+    cardTitle.classList.add("card-title")
+    cardTitle.innerText = producto.nombre
+    cardBody.appendChild(cardTitle)
+
+    // descripcion de la card
+    let description = document.createElement("p")
+    description.classList.add("card-text")
+    description.innerText = producto.descripcion
+    cardBody.appendChild(description)
+
+    // contenedor de precios
+    let priceWrapper = document.createElement("div")
+    priceWrapper.classList.add("price-content")
+    cardBody.appendChild(priceWrapper)
+
+    // precios
+    let price1 = document.createElement("span")
+    let price2 = document.createElement("span")
+
+    price1.innerText = formatoMoneda(producto.precio)
+    price2.innerText = formatoMoneda(producto.precio - (producto.precio * producto.descuento / 100))
+    priceWrapper.append(price1, price2)
+
+    // valoracion
+    let rating = valoracionProducto(producto.valoracion)
+    cardBody.appendChild(rating)
+
+    // boton
+    let buttonWrapper = document.createElement("div")
+    buttonWrapper.classList.add("d-grid")
+
+    let button = document.createElement("button")
+    button.classList.add("btn")
+    button.classList.add("btn-primary")
+    button.innerText = "Agregar al carrito"
+    button.addEventListener("click", function () {
+        onProductClick(producto)
+    })
+    buttonWrapper.appendChild(button)
+
+    cardBody.appendChild(buttonWrapper)
+
+
+    // añadimos la card a la grilla de productos
+    grillaProductos.appendChild(card)
+}
+
+function formatoMoneda(numero) {
+    const formatter = Intl.NumberFormat("es-CO", {
+        style: "currency",
+        currency: "COP",
+        currencyDisplay: 'code',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    })
+    return formatter.format(numero)
+}
+
+
+function onProductClick(producto) {
+   agregarProducto(producto)
 }
 
 
