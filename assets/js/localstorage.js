ac958c0c0f385a6f1ed77aaf5274fb4469268fec
Zulangy <100803602+ZulangySatizabal@users.noreply.github.com>
2024-11-14 12:43:25 +0000

    implementar lógica de carrito y renderizado de productos desde JSON

---
+++ assets/js/localstorage.js
@@ -0,0 +1,11 @@
+let carrito = obtenerProductos()
+function obtenerProductos() {
+    return JSON.parse(localStorage.getItem("carrito")) ?? [];
+}
+function agregarProducto(producto) {
+    localStorage.setItem("carrito", JSON.stringify(producto));
+}
+export {
+    carrito,
+    agregarProducto,
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,113 @@
-let productos = [
-    {
-        id: 1,
-        imagen: "../img/productos/blusa.jpg",
-        nombre: "T-shirt pintura interior bodegón",
-        descripcion : "Camisetas con diseño de teñido anudado en tonos naranja y rojo. Perfectas para cualquier estilo.",
-        precio: 85.000,
-        descuento : 50,
-        valoracion: 5
-    }
-]
+import productos from "./productos.json" with {type: "json"};
+import {agregarProducto, carrito} from "../assets/js/localstorage.js";
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Se referencia al section que contiene los productos
-    let grillaProductos = document.querySelector("#products-grid");
-
-    // Lógica de la card
-    // creamos el contenedor de la card
-    let card = document.createElement("div");
-    card.classList.add("card");
-
-    //creamos la imagen
-    let img = document.createElement("img");
-    img.src = productos[0].imagen;
-    img.classList.add("card-img-top");
-    card.appendChild(img);
-
-    //cuerpo de la card
-    let cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    card.appendChild(cardBody);
-
-    //titulo
-    let cardTitle = document.createElement("h5");
-    cardTitle.classList.add("card-title");
-    cardTitle.textContent = productos[0].nombre;
-    cardBody.appendChild(cardTitle);
-
-    // descripción del producto
-    let cardDescription = document.createElement("p");
-    cardDescription.classList.add("card-text");
-    cardDescription.textContent = productos[0].descripcion;
-    cardBody.appendChild(cardDescription);
-
-    // contenedor precio
-    let priceWrapper = document.createElement("div");
-    priceWrapper.classList.add("price-content");
-    cardBody.appendChild(priceWrapper);
-
-    // precios
-    let price1 = document.createElement("span");
-    let price2 = document.createElement("span");
-
-    price1.textContent = `$${productos[0].precio}`;
-    price2.textContent = `$${productos[0].precio - productos[0].descuento}`;
-
-    priceWrapper.append(price1, price2);
-
-    // valoración
-    let ratingWrapper = document.createElement("div");
-    ratingWrapper.classList.add("star-content");
-
-    for (let i = 0; i < 5; i++) {
-        let star = document.createElement("i");
-        star.classList.add("fas", "fa-star");
-        ratingWrapper.appendChild(star);
-    }
-
-    cardBody.appendChild(ratingWrapper);
-
-    // Botón de compra
-
-    let buttonWrapper = document.createElement("div");
-    buttonWrapper.classList.add("d-grid");
-
-    let button = document.createElement("button");
-    button.classList.add("btn", "btn-primary");
-    button.textContent = "Agregar al carrito";
-
-    buttonWrapper.appendChild(button);
-
-    cardBody.appendChild(buttonWrapper);
-
-
-
-    //añadimos la card a la grilla de productos
-    grillaProductos.appendChild(card);
+document.addEventListener("DOMContentLoaded", function () {
+  for (let producto of productos) {
+    dibujarTarjeta(producto);
+  }
 });
+
+function dibujarTarjeta(producto) {
+  // Se referencia al section que contiene los productos
+  let grillaProductos = document.querySelector("#products-grid");
+
+  // Lógica de la card
+  // creamos el contenedor de la card
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  //creamos la imagen
+  let img = document.createElement("img");
+  img.src = producto.imagen;
+  img.classList.add("card-img-top");
+  card.appendChild(img);
+
+  //cuerpo de la card
+  let cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  card.appendChild(cardBody);
+
+  //titulo
+  let cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = producto.nombre;
+  cardBody.appendChild(cardTitle);
+
+  // descripción del producto
+  let cardDescription = document.createElement("p");
+  cardDescription.classList.add("card-text");
+  cardDescription.textContent = producto.descripcion;
+  cardBody.appendChild(cardDescription);
+
+  // contenedor precio
+  let priceWrapper = document.createElement("div");
+  priceWrapper.classList.add("price-content");
+  cardBody.appendChild(priceWrapper);
+
+  // precios
+  let price = document.createElement("span");
+  let discount = document.createElement("span");
+
+  price.innerText = formatoMoneda(producto.precio);
+  discount.innerText = formatoMoneda(producto.precio - (producto.precio * producto.descuento) / 100);
+
+  priceWrapper.append(price, discount);
+
+  // valoración
+  let rating = valoracionProducto(producto.valoracion);
+  cardBody.appendChild(rating);
+
+  // Botón de compra
+  let buttonWrapper = document.createElement("div");
+  buttonWrapper.classList.add("d-grid");
+
+  let button = document.createElement("button");
+  button.classList.add("btn", "btn-primary");
+  button.innerHTML = "Agregar al carrito";
+
+  button.addEventListener("click", function () {
+    onProductClick(producto);
+  });
+
+  buttonWrapper.appendChild(button);
+  cardBody.appendChild(buttonWrapper);
+
+  //añadimos la card a la grilla de productos
+  grillaProductos.appendChild(card);
+};
+
+function formatoMoneda(numero) {
+  const formatter = Intl.NumberFormat("es-CO", {
+      style: "currency",
+      currency: "COP",
+      currencyDisplay: 'code',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+  });
+  return formatter.format(numero);
+};
+
+function valoracionProducto(numero) {
+  let ratingWrapper = document.createElement("div");
+  ratingWrapper.classList.add("star-content");
+
+  for (let i = 0; i < 5; i++) {
+    let star = document.createElement("i");
+
+    if (i < numero) {
+      star.classList.add("fa-solid");
+    } else {
+      star.classList.add("fa-regular");
+    };
+
+    star.classList.add("fa-star");
+
+    ratingWrapper.appendChild(star);
+  };
+
+  return ratingWrapper;
+};
+
+function onProductClick(producto) {
+  agregarProducto(producto);
+};
